c0bcc355ff203cf10f92e801318f753216189f03
RSShah2016 <shahruchi1986@gmail.com>
2020-09-29 22:34:06 +0100

    Add files via upload

---
+++ IDataRepository.cs
@@ -0,0 +1,18 @@
+﻿using InsiteTeamTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsiteTeamTask.Repositories
+{
+    public interface IDataRepository
+    {
+        List<Attendance> GetAttendanceListFor(int gameNumber, int seasonNumber);
+        List<Attendance> GetAttendanceListFor(string productCode);
+        List<Game> GetGames();
+        List<Product> GetProducts();
+        List<Event> GetEvents();
+        List<Season> GetSeasons(int eventId);
+    }
+}
